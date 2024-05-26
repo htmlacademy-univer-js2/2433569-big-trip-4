6d846a6292dbd7aa2096abce034d0e636224d143
MeBelkaMe <milka_marina94@mail.ru>
2024-05-26 17:59:15 +0500

    фикс

--- src/api-service/points-api.js
+++ src/api-service/points-api.js
@@ -6,10 +6,12 @@ export default class PointsApiService extends ApiService {
     return this._load({url: 'points'})
       .then(ApiService.parseResponse);
   }
+  
   get offers() {
     return this._load({ url: 'offers' })
       .then(ApiService.parseResponse);
-  } 
+  }
+
   get destinations() {
     return this._load({ url: 'destinations' })
       .then(ApiService.parseResponse);
